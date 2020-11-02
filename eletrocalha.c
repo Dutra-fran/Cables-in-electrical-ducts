783b76b007ae96c9923ab7c15a7e34aa1c71b77b
Dutra-fran <63562657+Dutra-fran@users.noreply.github.com>
2020-11-02 13:43:00 -0300

    Update eletrocalha.c
    Melhorando a codificação do arquivo.

--- eletrocalha.c
+++ eletrocalha.c
@@ -1,15 +1,17 @@
 /*
 Criado dia: 28/05/2019
-Última modificação: 04/06/2019
 Créditos: Robson Dutra
-Contato pra melhorarias: (85)985407927
 */
 
 #include <stdio.h>
-
 int main(){
     float AreaEletrocalha, Largura, Altura;
     float AreaCabo, pi, Raio;
+    float DiametroNominal;
+    float TaxaX;
+    int cont=10000;
+    int QuantCabos=0;
+    int opcoes=0;
 
     pi = 3.1415;    
 
@@ -27,30 +29,30 @@ int main(){
 
     printf("\n---------------");
     printf("\n\tCabos :V");
-    float DiametroNominal;
+    
     printf("\nDigite o Diametro nominal: ");
     scanf("%f", &DiametroNominal);
+
     Raio = DiametroNominal / 2;
-    int QuantCabos=0;
     
     AreaCabo = pi * (Raio * Raio);
 
     printf("\n---------------");
     printf("\n\tTaxa de ocupação dos cabos na eletrocalha :V\n");
-    float TaxaX;
-    int cont=10000;
+    
     printf("\n\nopções abaixo:");
     
-    int opcoes=0;
     while(cont > 0){
-    --cont;
-    QuantCabos++;
-    TaxaX = (QuantCabos * AreaCabo) / AreaEletrocalha;
-    if(TaxaX >= 0.4 && TaxaX <= 0.6){
-            opcoes++;
-        printf("\n\t%d. Taxa de ocupação: %1.2f\n", opcoes, TaxaX);
-        printf("\n\tQuantidades de Cabos que passam pela Eletrocalha: %d\n", QuantCabos);
-}
-}
+       --cont;
+       QuantCabos++;
+
+       TaxaX = (QuantCabos * AreaCabo) / AreaEletrocalha;
+
+       if(TaxaX >= 0.4 && TaxaX <= 0.6){
+           opcoes++;
+           printf("\n\t%d. Taxa de ocupação: %1.2f\n", opcoes, TaxaX);
+           printf("\n\tQuantidades de Cabos que passam pela Eletrocalha: %d\n", QuantCabos);
+       }
+    }
     return 0;
 }
